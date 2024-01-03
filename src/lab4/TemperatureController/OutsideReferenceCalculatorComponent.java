02eae31c006e5c98dd2910025e6abfe042feab54
stefaniacotan <stefaniacotan1@gmail.com>
2024-01-03 10:52:32 +0200

    laboratory 4

---
+++ src/lab4/TemperatureController/OutsideReferenceCalculatorComponent.java
@@ -0,0 +1,80 @@
+package lab4.TemperatureController;
+
+import core.FuzzyPetriLogic.Executor.AsyncronRunnableExecutor;
+import core.FuzzyPetriLogic.FuzzyDriver;
+import core.FuzzyPetriLogic.FuzzyToken;
+import core.FuzzyPetriLogic.PetriNet.FuzzyPetriNet;
+import core.FuzzyPetriLogic.PetriNet.Recorders.FullRecorder;
+import core.FuzzyPetriLogic.Tables.OneXOneTable;
+import core.TableParser;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class OutsideReferenceCalculatorComponent {
+    static String reader = "" +
+            "{[<ZR,NL><ZR,NM><ZR,ZR><ZR,PM><ZR,PL>]" +
+            " [<ZR,NL><ZR,NM><ZR,ZR><ZR,PM><ZR,PL>]" +
+            " [<ZR,NL><ZR,NM><ZR,ZR><ZR,PM><ZR,PL>]" +
+            " [<ZR,NL><ZR,NM><ZR,ZR><ZR,PM><ZR,PL>]" +
+            " [<ZR,NL><ZR,NM><ZR,ZR><ZR,PM><ZR,PL>]}";
+    static String t2Table = "{[<PL><PM><ZR><NM><NL>]}";
+
+    private FuzzyPetriNet net;
+    private int p1OustideTemInp;
+    private int t2Out;
+    private FuzzyDriver outsideTempDriver;
+    private FuzzyDriver tankWaterTemeDriver;
+    private FullRecorder rec;
+    private AsyncronRunnableExecutor execcutor;
+
+    public OutsideReferenceCalculatorComponent(Plant plant, HeaterTankControllerComponent comp, long simPeriod) {
+        TableParser parser = new TableParser();
+        net = new FuzzyPetriNet();
+
+        int p0 = net.addPlace();
+        net.setInitialMarkingForPlace(p0, FuzzyToken.zeroToken());
+
+        p1OustideTemInp = net.addInputPlace();
+
+        int t0 = net.addTransition(0, parser.parseTwoXTwoTable(reader));
+        net.addArcFromPlaceToTransition(p0, t0, 1.0);
+        net.addArcFromPlaceToTransition(p1OustideTemInp, t0, 1.0);
+
+        int p2 = net.addPlace();
+        net.addArcFromTransitionToPlace(t0, p2);
+
+        int p3 = net.addPlace();
+        net.addArcFromTransitionToPlace(t0, p3);
+
+        int t1delay = net.addTransition(1, parser.parseOneXOneTable(t2Table));
+        net.addArcFromPlaceToTransition(p2, t1delay, 1.0);
+        net.addArcFromTransitionToPlace(t1delay, p0);
+
+        t2Out = net.addOuputTransition(OneXOneTable.defaultTable());
+        outsideTempDriver = FuzzyDriver.createDriverFromMinMax(-30, 10);
+        tankWaterTemeDriver = FuzzyDriver.createDriverFromMinMax(45, 68);
+        net.addActionForOuputTransition(t2Out, tk -> {
+            comp.setWaterRefTemp(tankWaterTemeDriver.defuzzify(tk));
+        });
+        net.addArcFromPlaceToTransition(p3,t2Out,1.0);
+        rec = new FullRecorder();
+        execcutor = new AsyncronRunnableExecutor(net, simPeriod);
+        execcutor.setRecorder(rec);
+    }
+    public void start() {
+        (new Thread(execcutor)).start();  }
+    public void stop() {    execcutor.stop();  }
+
+    public void setOutsideTemp(double waterRefTemp) {
+        Map<Integer, FuzzyToken> inps = new HashMap<Integer, FuzzyToken>();
+        inps.put(p1OustideTemInp, outsideTempDriver.fuzzifie(waterRefTemp));
+        execcutor.putTokenInInputPlace(inps);  }
+
+    public FuzzyPetriNet getNet() {    return net;  }
+    public FullRecorder getRecorder() {    return rec;  }
+
+
+
+}
